209865dbb9bae273fe3ce1d1da5141f8038387ec
Wanderson <lopez.wanderson@gmail.com>
2022-05-03 12:16:44 -0600

    Se crea la primera celda

--- Today/ViewController.swift
+++ Today/ViewController.swift
@@ -8,14 +8,26 @@
 import UIKit
 
 class ReminderListViewController: UICollectionViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let listLayout = listLayout()
         collectionView.collectionViewLayout = listLayout
+        
+        // Cell registration specifies how to configure the content
+        // and appearance of a cell
+        let cellRegistration = UICollectionView.CellRegistration { (cell: UICollectionViewListCell, indexPath: IndexPath, itemIdentifier: String) in
+            let reminder = Reminder.sampleData[indexPath.item]
+            
+            // defaultContentConfiguration() creates a content
+            // configuration with the predefined system style
+            var contentConfiguration = cell.defaultContentConfiguration()
+            contentConfiguration.text = reminder.title
+            cell.contentConfiguration = contentConfiguration
+        }
     }
-
+    
     private func listLayout() -> UICollectionViewCompositionalLayout {
         var listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
         listConfiguration.showsSeparators = false
