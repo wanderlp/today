e9c9bec838cece41d9a89e230c3966cd4843eae8
Wanderson <lopez.wanderson@gmail.com>
2022-05-03 11:51:53 -0600

    Se agregó la función listLayout

--- Today/ViewController.swift
+++ Today/ViewController.swift
@@ -7,13 +7,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ReminderListViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    private func listLayout() -> UICollectionViewCompositionalLayout {
+        var listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
+        listConfiguration.showsSeparators = false
+        listConfiguration.backgroundColor = .clear
+        return UICollectionViewCompositionalLayout.list(using: listConfiguration)
+    }
 }
-
