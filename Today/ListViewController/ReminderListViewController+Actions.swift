b4b89549d4860b609fa14114dde88729f65f1e17
Wanderson <lopez.wanderson@gmail.com>
2022-05-05 18:37:17 -0600

    Se completa el reminder en el evento didPressDoneButton

--- Today/ListViewController/ReminderListViewController+Actions.swift
+++ Today/ListViewController/ReminderListViewController+Actions.swift
@@ -10,6 +10,7 @@ import UIKit
 extension ReminderListViewController {
     // The @objc attribute makes this method available to Objective-C code
     @objc func didPressDoneButton(_ sender: ReminderDoneButton) {
-        
+        guard let id = sender.id else { return }
+        completeReminder(with: id)
     }
 }
