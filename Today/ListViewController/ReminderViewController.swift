fd43469c201d63158cf1d72866ce3f8dec5aae6d
Wanderson <lopez.wanderson@gmail.com>
2022-05-09 14:02:32 -0600

    Se creó la clase ReminderViewController

---
+++ Today/ListViewController/ReminderViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ReminderViewController.swift
+//  Today
+//
+//  Created by Wanderson López on 9/05/22.
+//
+
+import UIKit
+
+class ReminderViewController: UICollectionViewController {
+    
+}
