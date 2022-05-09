9098686635f792d941c554eca2eabc377023c0a5
Wanderson <lopez.wanderson@gmail.com>
2022-05-09 14:57:45 -0600

    Se mueve de lugar la función Text

--- Today/DetailViewController/ReminderViewController+Row.swift
+++ Today/DetailViewController/ReminderViewController+Row.swift
@@ -36,13 +36,4 @@ extension ReminderViewController {
             }
         }
     }
-    
-    func text(for row: Row) -> String? {
-        switch row {
-        case .viewDate:  return reminder.dueDate.dayText
-        case .viewNotes: return reminder.notes
-        case .viewTime:  return reminder.dueDate.formatted(date: .omitted, time: .shortened)
-        case .viewTitle: return reminder.title
-        }
-    }
 }
